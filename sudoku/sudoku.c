4c7d8caaa25e329c8ecec9e0c322a39c7b5eb599
pzhaoyang <pzhaoyang@126.com>
2018-05-16 17:43:33 +0800

    add colum&row

--- sudoku/sudoku.c
+++ sudoku/sudoku.c
@@ -17,11 +17,20 @@
 
 */
 
+#define DEBUG 1
+#define SPACE_13 "             "
+#define SPACE_3 "   "
+
 #define SIZE 9
+
+typedef struct result{
+    char value[SIZE]; //if the value is confirmed, the value[0] will be the confirmed value, and others will be 0.
+    int confirmed; //0 is not confirmed, 1 is confirmed
+}result_t;
 
 typedef struct sudoku {
     char template[SIZE][SIZE];
-    char value[SIZE][SIZE];
+    result_t sresult[SIZE][SIZE];
 }sudoku_t;
 
 static sudoku_t issue;
@@ -47,20 +56,30 @@ void hexdump(char *p, int size){
     }
     printf("\n");
 }
-void print_sudoku(char p[9][9]){
-    int c,r;
+void print_sudoku(result_t p[9][9]){
+    int c,r,v;
     if( p == NULL){
         printf("\n******\nThe sudoku is invalid!\n******\n");
         return;
     }
 
     printf("\nThe Result:\n");
-    for(c=0; c<9; c++){
-        for(r=0; r<9; r++){
-            printf("%c ", p[c][r]);
+    for(r=0; r<SIZE; r++){
+        for(c=0; c<SIZE; c++){
+            if(p[r][c].confirmed == 1){
+                printf("%c%s", p[r][c].value[0], SPACE_13);
+            }else{
+                printf("{");
+                for(v=0; v<SIZE; v++){
+                    printf("%c", p[r][c].value[v]);
+                }
+                printf("}%s", SPACE_3);
+            }
+            
         }
         printf("\n");
     }
+    printf("\n");
 }
 
 int read_template(){
@@ -81,18 +100,145 @@ int read_template(){
             printf("  %s\n", line);
         }
     }
-    printf("}\n");
+    printf("}\n\n");
 
     fclose(fp);
 
     return 0;
 }
 
+void resolve_init(){
+    int x, y, z;
+    #if DEBUG
+        printf("resolve_init start:\n");
+    #endif
+    for(x=0; x<SIZE; x++){
+        for(y=0; y<SIZE; y++){
+            if(issue.template[x][y] != '0' ){ 
+                issue.sresult[x][y].value[0] = issue.template[x][y];
+                issue.sresult[x][y].confirmed = 1;
+                #if DEBUG
+                    printf("%c(%d) ", issue.sresult[x][y].value[0], issue.sresult[x][y].confirmed);
+                #endif
+                continue;
+            }
+            
+            issue.sresult[x][y].confirmed = 0;
+            #if DEBUG
+                printf("0(%d) ", issue.sresult[x][y].confirmed);
+            #endif
+            for(z=0; z<SIZE; z++){
+               issue.sresult[x][y].value[z] = z + '1';
+            }
+        }
+        #if DEBUG
+            printf("\n");
+        #endif
+    }
+    #if DEBUG
+        printf("resolve_init end.\n");
+    #endif
+}
+
+int update_confirm(int row, int colum){
+    int statistics, locate, z;
+    result_t (*p)[SIZE] = issue.sresult;
+    
+    //statistics and then to update confirmed value
+    for(statistics=0, z=0; z<SIZE; z++){
+        if(p[row][colum].value[z] != '0'){
+            statistics++;
+            locate = z;
+        }
+    }
+    if(statistics == 1){
+        // fixed a cell number
+        p[row][colum].value[0] = p[row][colum].value[locate];
+        p[row][colum].confirmed = 1;
+        
+        return 0;
+    }
+    return -1;
+}
+
+void cell_compare(int need_compared_row, int need_compared_colum){
+    int r, c, s, z;
+    result_t (*p)[SIZE] = issue.sresult;
+    
+    for(z=0; z<SIZE; z++){
+        if(p[need_compared_row][need_compared_colum].value[z] == '0') continue;
+        //row compare r is fixed. acture is colum compare
+        for(c=0; c<SIZE; c++){
+            if(p[need_compared_row][c].confirmed != 1) continue;
+
+            if(p[need_compared_row][c].value[0] == p[need_compared_row][need_compared_colum].value[z]){
+                p[need_compared_row][need_compared_colum].value[z] = '0'; //if this is equal, then remove it using '0'
+            }
+        }
+        
+        //colum compare c is fixed, acture is row compare
+        for(r=0; r<SIZE; r++){
+            if(p[r][need_compared_colum].confirmed != 1) continue;
+
+            if(p[r][need_compared_colum].value[0] == p[need_compared_row][need_compared_colum].value[z]){
+                p[need_compared_row][need_compared_colum].value[z] = '0'; //if this is equal, then remove it using '0'
+            }
+        }
+        
+        //small square compare
+/*        r = need_compared_row%3;
+        c = need_compared_colum%3;
+        switch
+        for(s=0; s<SIZE; r++){
+            if(p[r][c].confirmed != 1) continue;
+
+            if(p[r][need_compared_colum].value[0] == p[need_compared_row][need_compared_colum].value[z]){
+                p[need_compared_row][need_compared_colum].value[z] = '0'; //if this is equal, then remove it using '0'
+            }
+        }
+*/
+    }
+}
+
+
+void exclude_exist_num(){
+    int x, y;
+    result_t (*p)[SIZE] = issue.sresult;
+    
+    if( p == NULL) return;
+   
+    for(x=0; x<SIZE; x++){
+        for(y=0; y<SIZE; y++){
+            if( p[x][y].confirmed == 1){
+               continue;
+            }
+            cell_compare(x, y);
+            if(0 == update_confirm(x, y)){
+                exclude_exist_num();
+            }
+        }
+    }
+    
+    
+}
+
+void resolve(){
+    resolve_init();
+    exclude_exist_num();
+}
+
+void init(){
+    memset(&issue, 0, sizeof(issue));
+}
+
 
 int main(int argc, char* argv[]){
+   init();
    
    read_template();
 
-   print_sudoku(issue.template);
+   resolve();
+   
+   print_sudoku(issue.sresult);
    return 0;  
 }
